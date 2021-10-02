d460b15334de779376ada2ca0e60cc025a14f382
Moh Dev <mezmoh530@gmail.com>
2021-10-02 14:50:13 +0100

    add activity settings and fix bug

--- app/src/main/java/com/example/Company/Company_activity.kt
+++ app/src/main/java/com/example/Company/Company_activity.kt
@@ -4,20 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.Data.PrefManage
 import com.example.Pojo.Companies
-import com.example.Products.ProductsViewModel
-import com.example.Products.Products_activity
+import com.example.Settings.Settings
 import com.example.phons.R
-
-
 
 
 class Company_activity : AppCompatActivity() {
@@ -28,6 +26,9 @@ class Company_activity : AppCompatActivity() {
     var adapter:CompanyAdapter? = null
     var progress:ProgressBar? = null
     var tv_loading:TextView? = null
+    var bu_go_to_settengs:CardView? = null
+    var et_searchbar:EditText? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,9 +44,19 @@ class Company_activity : AppCompatActivity() {
 
         tv_loading = findViewById(R.id.tv_loading)
 
+        et_searchbar = findViewById(R.id.et_searchbar)
+
+        et_searchbar!!.isEnabled = false
+
         CompanyViewModel = CompanyViewModel()
 
         CompanyViewModel!!.getcompany(uiq)
+
+        bu_go_to_settengs = findViewById(R.id.bu_gotoseting)
+
+        bu_go_to_settengs!!.setOnClickListener {
+            startActivity(Intent(this ,Settings::class.java))
+        }
 
         adapter = CompanyAdapter(this)
 
@@ -62,6 +73,7 @@ class Company_activity : AppCompatActivity() {
 
         CompanyViewModel!!.MutableLiveDataCompaby.observe(this ,object : Observer<ArrayList<Companies>>{
             override fun onChanged(t: ArrayList<Companies>?) {
+                et_searchbar!!.isEnabled = true
                 progress!!.visibility = View.GONE
                 tv_loading!!.visibility = View.GONE
                 adapter!!.setList(t!!)
